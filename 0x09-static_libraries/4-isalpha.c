92415ae0bf1b7b4c64692a99eabaa721d6abec63
obakeng <obakengmakgale13@gmail.com>
2023-04-10 12:32:34 -0700

    New updates

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: character that will be checked
+ *
+ * Return: 1 if c is a letter, 0 if not
+ */
+
+int _isalpha(int c)
+
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
