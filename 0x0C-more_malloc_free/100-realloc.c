ef9b1e84f7883b367b8e5e16a1481817a5954a74
obakeng <obakengmakgale13@gmail.com>
2023-04-26 12:36:50 -0700

    New updates

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,15 +17,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int a;
 
 	if (new_size == old_size)
-		return (point);
+		return (ptr);
 
-	if (new_size == 0 && point)
+	if (new_size == 0 && ptr)
 	{
-		free(point);
+		free(ptr);
 		return (NULL);
 	}
 
-	if (!point)
+	if (!ptr)
 		return (malloc(new_size));
 
 	point = malloc(new_size);
