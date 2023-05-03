e1b2290cd829a54a4312555e2ea5399f25ce308d
obakeng <obakengmakgale13@gmail.com>
2023-05-03 10:23:22 -0700

    New updates

---
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes c to stdout
+ * @c: character
+ * Return: 1 on success, -1 on error
+ */
+
+int _putchar(char c)
+
+{
+	return (write(1, &c, 1));
+}
